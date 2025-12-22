50f74ee134ed5ae64ca5fd9cb86880138d741af8
kostas <kostaslamprounk@gmail.com>
2025-12-22 17:57:42 +0100

    reset und delete btn ausgefertigt

--- app/main.js
+++ app/main.js
@@ -21,8 +21,15 @@ task.addEventListener('keydown', (e) => {
         const deleteButton = document.createElement('button');
         
         deleteButton.classList.add('delete-btn');
-        deleteButton.textContent = "X";
+        deleteButton.classList.add('icon-btn');
+        deleteButton.innerHTML = `
+        
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+            <path d="M5.28 4.22a.75.75 0 0 0-1.06 1.06L6.94 8l-2.72 2.72a.75.75 0 1 0 1.06 1.06L8 9.06l2.72 2.72a.75.75 0 1 0 1.06-1.06L9.06 8l2.72-2.72a.75.75 0 0 0-1.06-1.06L8 6.94 5.28 4.22Z" />
+            </svg>
 
+        `;
+        
 
         //Das Element ist erschaffen worden - wichtig!
         //Der EventListener ist das Gehirn des Buttons, würdest du es außerhalb
